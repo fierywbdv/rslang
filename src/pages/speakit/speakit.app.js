f2a4a5e8df685f1e6378754ed22d669d9fc8936b
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-06-22 21:32:51 +0300

    feat(#14): create main function to start the game

--- src/pages/speakit/speakit.app.js
+++ src/pages/speakit/speakit.app.js
@@ -1,1 +1,14 @@
+import './scss/main.scss';
 
+import Controller from './app/controller/controller';
+import Model from './app/model/model';
+import View from './app/view/view';
+
+export default function speakit() {
+  const model = new Model();
+  const view = new View();
+  const controller = new Controller(model, view);
+  view.init(controller);
+  model.init(controller);
+  controller.init();
+}

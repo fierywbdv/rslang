f3ef7153b9a9c63bd774ec2163135236a2766246
missukevitch <missukevitch@gmail.com>
2020-06-23 16:40:15 +0300

    feat(#22): add switches

--- src/pages/sprint/components/game-screen-component.js
+++ src/pages/sprint/components/game-screen-component.js
@@ -3,6 +3,12 @@ import { timeChartComponent } from './time-chart-component';
 export const gameScreenComponent = () => {
   const gameScreen = `<div class="game-screen">
                             <div class="game-screen__content">
+                                <div class="switches">
+                                    <label for="level">Level</label>
+                                    <input id="level" name="level" value="1" type="number" min="1" max="6" required>
+                                    <label for="page">Page</label>
+                                    <input id="page" name="page" value="1" type="number" min="1" max="60" required>
+                                </div>
                                 <div class="current-state">
                                     <span class="points">100</span>${timeChartComponent()}
                                 </div>

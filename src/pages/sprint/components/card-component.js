b6b42f24e40097768391b0362c118a46e35296ee
missukevitch <missukevitch@gmail.com>
2020-06-24 15:28:34 +0300

    feat(#22): add media queries

---
+++ src/pages/sprint/components/card-component.js
@@ -0,0 +1,23 @@
+export const cardComponent = () => {
+  const card = `<div class="card">
+                    <div class="card__header">
+                        <div class="card__circles">
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                        </div>
+                        <p class="points-increase">+80 очков за слово</p>
+                    </div>
+                    <div class="card__main">
+                        <p class="card__word">world</p>
+                        <p class="card__translation">мир</p>
+                        <div class="card__result"><i class="fas fa-check"></i></div>
+                        <hr>
+                    </div>
+                    <div class="card__footer">
+                        <button type="button" class="btn btn-danger">Неверно</button>
+                        <button type="button" class="btn btn-success">Верно</button>
+                    </div>
+                </div>`;
+  return card;
+};
--- src/pages/sprint/components/game-screen-component.js
+++ src/pages/sprint/components/game-screen-component.js
@@ -1,37 +1,22 @@
 import { timeChartComponent } from './time-chart-component';
+import { cardComponent } from './card-component';
 
 export const gameScreenComponent = () => {
   const gameScreen = `<div class="game-screen">
                             <div class="game-screen__content">
-                                <div class="switches">
-                                    <label for="level">Level</label>
-                                    <input id="level" name="level" value="1" type="number" min="1" max="6" required>
-                                    <label for="page">Page</label>
-                                    <input id="page" name="page" value="1" type="number" min="1" max="60" required>
+                                <div class="choice">
+                                    <div class="switches">
+                                        <label for="level">Level</label>
+                                        <input id="level" name="level" value="1" type="number" min="1" max="6" required>
+                                        <label for="page">Page</label>
+                                        <input id="page" name="page" value="1" type="number" min="1" max="60" required>
+                                    </div>
+                                    <span>Изученные слова</span>
                                 </div>
                                 <div class="current-state">
                                     <span class="points">100</span>${timeChartComponent()}
                                 </div>
-                                <div class="card">
-                                    <div class="card__header">
-                                        <div class="card__circles">
-                                            <div class="circle"></div>
-                                            <div class="circle"></div>
-                                            <div class="circle"></div>
-                                        </div>
-                                        <p class="points-increase">+80 очков за слово</p>
-                                    </div>
-                                    <div class="card__main">
-                                        <p class="card__word">world</p>
-                                        <p class="card__translation">мир</p>
-                                        <div class="card__result"><i class="fas fa-check"></i></div>
-                                        <hr>
-                                    </div>
-                                    <div class="card__footer">
-                                        <button type="button" class="btn btn-danger">Неверно</button>
-                                        <button type="button" class="btn btn-success">Верно</button>
-                                    </div>
-                                </div>
+                                ${cardComponent()}
                                 <div class="arrows">
                                     <div class="arrow"><i class="fas fa-long-arrow-alt-left"></i></div>
                                     <div class="arrow"><i class="fas fa-long-arrow-alt-right"></i></div>

941d1d9f4b74b854f57ce7e5f92eb3a5ddf48e62
GoldenkovVitali <vitalik.resscuer@mail.ru>
2020-07-01 21:27:47 +0300

    feat(#16):  change structure app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import main from './pages/main/main.app';
-import audiocall from './pages/audiocall/audiocall.app';
 import englishPuzzle from './pages/english-puzzle/english-puzzle.app';
 import ourgame from './pages/ourgame/ourgame.app';
 import savanna from './pages/savanna/savanna.app';
@@ -8,7 +7,6 @@ import router from './router/Router';
 
 window.onload = () => {
   main.init();
-  audiocall.init();
   englishPuzzle.init();
   ourgame.init();
   savanna.init();

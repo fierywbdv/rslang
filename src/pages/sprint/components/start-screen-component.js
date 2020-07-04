571c0839717cb908fb86eb001369f1e06f382f03
missukevitch <missukevitch@gmail.com>
2020-07-04 10:40:21 +0300

    fix(#33): add class for h1

--- src/pages/sprint/components/start-screen-component.js
+++ src/pages/sprint/components/start-screen-component.js
@@ -2,7 +2,7 @@ export const startScreenComponent = () => {
   const startScreen = `<div class="sprint-wrapper bg">
                           <div class="start-screen">
                             <div class="start-screen__content">
-                                <h1>Спринт</h1>
+                                <h1 class="title">Спринт</h1>
                                 <p>Укажите, принадлежит ли перевод слову на английском языке.</p>
                                 <button type="button" class="btn btn-lg button-start">Начать игру</button>
                             </div>

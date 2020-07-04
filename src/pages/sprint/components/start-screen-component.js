c378af37d0aa8732e0fef7835bc5e86dab6fa3b7
missukevitch <missukevitch@gmail.com>
2020-07-04 11:51:40 +0300

    fix(#33): replace h1 with h2

--- src/pages/sprint/components/start-screen-component.js
+++ src/pages/sprint/components/start-screen-component.js
@@ -2,7 +2,7 @@ export const startScreenComponent = () => {
   const startScreen = `<div class="sprint-wrapper bg">
                           <div class="start-screen">
                             <div class="start-screen__content">
-                                <h1 class="title">Спринт</h1>
+                                <h2 class="title">Спринт</h2>
                                 <p>Укажите, принадлежит ли перевод слову на английском языке.</p>
                                 <button type="button" class="btn btn-lg button-start">Начать игру</button>
                             </div>

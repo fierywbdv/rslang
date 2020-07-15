12042d98185df4d4814b25f901962214a1b47a37
Dmitriy <krudda@yandex.ru>
2020-07-15 16:59:29 +0300

    feat(#84): add local statistic, add some responsive

--- src/pages/main/components/card/deleteWordHandler.js
+++ src/pages/main/components/card/deleteWordHandler.js
@@ -14,45 +14,54 @@ const deleteWordHandler = () => {
         const currentIterator = Number(button.getAttribute('id'));
         const card = document.querySelector(`#main-card-${currentIterator}`);
         const typeOfGame = localStorage.getItem('typeOfGame');
+        card.setAttribute('guessed', 'true');
 
-        let input;
+        if (!button.classList.contains('delete-disabled')) {
+          let input;
 
-        if (localStorage.getItem('userSetExplanation') === 'true') {
-          input = document.querySelector(`#to-write-${currentIterator}`);
-        } else {
-          input = document.querySelector(`#to-write-example-${currentIterator}`);
-        }
+          if (localStorage.getItem('userSetExplanation') === 'true') {
+            input = document.querySelector(`#to-write-${currentIterator}`);
+          } else if (localStorage.getItem('userSetExample') === 'true') {
+            input = document.querySelector(`#to-write-example-${currentIterator}`);
+          } else {
+            input = document.querySelector(`#to-write-word-${currentIterator}`);
+          }
 
-        const dataWord = {
-          wordId: input.getAttribute('data'),
-          word: input.placeholder,
-          wordDifficulty: 'false',
-          wordAudio: input.getAttribute('data-audio'),
-          wordTranslate: card.getAttribute('data-translate'),
-          wordImage: card.getAttribute('data-img'),
-        };
+          const dataWord = {
+            wordId: input.getAttribute('data'),
+            word: input.placeholder,
+            wordDifficulty: 'false',
+            wordAudio: input.getAttribute('data-audio'),
+            wordTranslate: card.getAttribute('data-translate'),
+            wordImage: card.getAttribute('data-img'),
+          };
 
-        input.classList.add('deleted');
-        if (typeOfGame === 'new') {
-          addToUserWords(dataWord, 'true');
-        }
-        if (typeOfGame === 'repeat') {
-          updateUserWords(dataWord, 'true');
-        }
-        if (typeOfGame === 'mix') {
-          updateMixUserWords(dataWord, 'true');
-        }
+          input.classList.add('deleted');
+          input.blur();
+          input.readOnly = true;
 
-        if (currentIterator + 1 === lastSlide) {
-          setTimeout(() => {
-            nextBTN.classList.add('main-btn-disable');
-            getNotation();
-          }, 1000);
-        } else {
-          setTimeout(() => {
-            nextBTN.click();
-            changeProgressBar(currentIterator, lastSlide);
-          }, 1000);
+          if (typeOfGame === 'new') {
+            addToUserWords(dataWord, 'true');
+          }
+          if (typeOfGame === 'repeat') {
+            updateUserWords(dataWord, 'true');
+          }
+          if (typeOfGame === 'mix') {
+            updateMixUserWords(dataWord, 'true');
+          }
+
+          if (currentIterator + 1 === lastSlide) {
+            setTimeout(() => {
+              nextBTN.classList.add('main-btn-disable');
+              getNotation();
+            }, 1000);
+          } else {
+            setTimeout(() => {
+              nextBTN.click();
+              changeProgressBar(currentIterator, lastSlide);
+              button.classList.add('delete-disabled');
+            }, 1000);
+          }
         }
       });
     });

85e58a344d12a1102d7b7ce67257e892fa1551c1
Vladimir Veraksa <vladimirveraksa@gmail.com>
2020-07-15 21:06:58 +0300

    fix: add text

--- src/pages/audiocall/components/modal-statistic/modal-statistic.js
+++ src/pages/audiocall/components/modal-statistic/modal-statistic.js
@@ -1,11 +1,11 @@
 const modalStatistic = (info) => {
-  const template = `<button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary">Form in simple modal </button>
+  const template = `<button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-primary">Все результаты</button>
                       <!-- Modal-->
                       <div id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" class="modal fade text-left">
                         <div role="document" class="modal-dialog">
                           <div class="modal-content">
                             <div class="modal-header">
-                              <h4 id="exampleModalLabel" class="modal-title">Signin Modal</h4>
+                              <h4 id="exampleModalLabel" class="modal-title">Все результаты</h4>
                               <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
                             </div>
                             <div class="modal-body">
